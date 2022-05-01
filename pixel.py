98fc0838fc450f45e0218f9b5af1455ae24325bd
njfe1 <86229342+njfe1@users.noreply.github.com>
2022-05-01 16:18:10 +0800

    Update pixel.py

--- pixel.py
+++ pixel.py
@@ -1,15 +1,32 @@
 from PIL import Image
+import os
 class Pixel():
-    def __init__(self , matrix_size):
-        self.im_address_list = []
+    def __init__(self):
         self.im_address = 0
-        self.matrix_size = matrix_size
+        self.matrix_size = 0
         self.pixel_rgb_list = []
         self.matrix_rgb_dict = {}
         self.matrix_pixel_max = []
     def user_input(self):
+        
         self.im_address = input('image address:')
+        sizelist = []
+        img = Image.open(self.im_address)
+        img_width , img_height = img.size
+        if img_width > img_height:
+            forrange = img_height
+        else:
+            forrange = img_width
+        for i in range(1 , forrange):
+            if img_height % i == 0 and img_width % i == 0:
+                sizelist.append(i)
+            else:
+                continue
+        print("size:",sizelist)
+        self.matrix_size = int(input("size:"))
     def impixel(self):
+        print(self.im_address)
+        print(self.matrix_size)
         img = Image.open(self.im_address)
         img = img.convert("RGB")
         img_width , img_height = img.size
@@ -20,7 +37,6 @@ class Pixel():
                 self.pixel_rgb_list.append(pixel_rgb)
         
         for matrix in range(0 , matrixnum):
-
             self.matrix_rgb_dict = {}   
             for a in range(int((matrix%(img_width/self.matrix_size))*self.matrix_size) , int((matrix%(img_width/self.matrix_size))*self.matrix_size + self.matrix_size)):
                 for b in range(int(matrix/(img_width/self.matrix_size))*self.matrix_size , int(matrix/(img_width/self.matrix_size))*self.matrix_size + self.matrix_size):
@@ -35,12 +51,9 @@ class Pixel():
             for draw_x in range(int((matrix_index%(img_width/self.matrix_size))*self.matrix_size) , int((matrix_index%(img_width/self.matrix_size))*self.matrix_size + self.matrix_size)):
                 for draw_y in range(int(matrix_index/(img_width/self.matrix_size))*self.matrix_size , int(matrix_index/(img_width/self.matrix_size))*self.matrix_size + self.matrix_size):
                     img.putpixel((draw_x,draw_y),rect_rgb)
-        img.save(self.im_address)
+        name , fileback = os.path.splitext(self.im_address)
+        img.save(name + "pixel3" + fileback)
+pixel = Pixel()
 if __name__ == "__main__":
-    pixel = Pixel(2)
     pixel.user_input()
     pixel.impixel()
-
-            
-                
-           
