bb1a274a1379862c5de34c803cc9761fddfd2094
njfe1 <86229342+njfe1@users.noreply.github.com>
2022-09-30 19:36:27 +0800

    Update pixel.py

--- pixel.py
+++ pixel.py
@@ -1,14 +1,17 @@
 from PIL import Image
 import os
+
 class Pixel():
+    initflag = False
     def __init__(self):
-        self.im_address = 0
-        self.matrix_size = 0
+        self.matrix_size = 1
         self.pixel_rgb_list = []
         self.matrix_rgb_dict = {}
         self.matrix_pixel_max = []
-    def user_input(self):
         
+    def user_input(self):
+        if self.initflag:
+            self.__init__()
         self.im_address = input('image address:')
         sizelist = []
         img = Image.open(self.im_address)
@@ -24,6 +27,7 @@ class Pixel():
                 continue
         print("size:",sizelist)
         self.matrix_size = int(input("size:"))
+        self.initflag = True
     def impixel(self):
         print(self.im_address)
         print(self.matrix_size)
@@ -38,22 +42,37 @@ class Pixel():
         
         for matrix in range(0 , matrixnum):
             self.matrix_rgb_dict = {}   
-            for a in range(int((matrix%(img_width/self.matrix_size))*self.matrix_size) , int((matrix%(img_width/self.matrix_size))*self.matrix_size + self.matrix_size)):
-                for b in range(int(matrix/(img_width/self.matrix_size))*self.matrix_size , int(matrix/(img_width/self.matrix_size))*self.matrix_size + self.matrix_size):
+            ast = int((matrix%(img_width/self.matrix_size))*self.matrix_size)
+            aed = int((matrix%(img_width/self.matrix_size))*self.matrix_size + self.matrix_size)
+            bst = int(matrix/(img_width/self.matrix_size))*self.matrix_size
+            bed = int(matrix/(img_width/self.matrix_size))*self.matrix_size + self.matrix_size
+            end0 = int((matrix%(img_width/self.matrix_size))*self.matrix_size + self.matrix_size)-1
+            end1 = int(matrix/(img_width/self.matrix_size))*self.matrix_size + self.matrix_size - 1
+            for a in range(ast, aed):
+                for b in range(bst, bed):
                     if self.pixel_rgb_list[b * img_width + a] not in self.matrix_rgb_dict:
                         self.matrix_rgb_dict[self.pixel_rgb_list[b * img_width + a]] = 1
                     elif self.pixel_rgb_list[b * img_width + a] in self.matrix_rgb_dict:
                         self.matrix_rgb_dict[self.pixel_rgb_list[b * img_width + a]] += 1
-                    if a == int((matrix%(img_width/self.matrix_size))*self.matrix_size + self.matrix_size) - 1 and b == int(matrix/(img_width/self.matrix_size))*self.matrix_size + self.matrix_size - 1:
+                    if a == end0 and b == end1:
                         self.matrix_pixel_max.append(max(self.matrix_rgb_dict))
 
         for matrix_index , rect_rgb in enumerate(self.matrix_pixel_max):
-            for draw_x in range(int((matrix_index%(img_width/self.matrix_size))*self.matrix_size) , int((matrix_index%(img_width/self.matrix_size))*self.matrix_size + self.matrix_size)):
-                for draw_y in range(int(matrix_index/(img_width/self.matrix_size))*self.matrix_size , int(matrix_index/(img_width/self.matrix_size))*self.matrix_size + self.matrix_size):
+            draw_xs = int((matrix_index%(img_width/self.matrix_size))*self.matrix_size)
+            draw_xe = int((matrix_index%(img_width/self.matrix_size))*self.matrix_size + self.matrix_size)
+            draw_ys = int(matrix_index/(img_width/self.matrix_size))*self.matrix_size
+            draw_ye = int(matrix_index/(img_width/self.matrix_size))*self.matrix_size + self.matrix_size
+            for draw_x in range(draw_xs, draw_xe):
+                for draw_y in range(draw_ys, draw_ye):
                     img.putpixel((draw_x,draw_y),rect_rgb)
         name , fileback = os.path.splitext(self.im_address)
-        img.save(name + "pixel3" + fileback)
+        img.save(name + "pixel" + fileback)
 pixel = Pixel()
 if __name__ == "__main__":
-    pixel.user_input()
-    pixel.impixel()
+    while 1:
+        pixel.user_input()
+        pixel.impixel()
+
+            
+                
+           
